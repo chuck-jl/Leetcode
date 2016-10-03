c36437b9ed6b396763d52fcec084d7dc683dea1d
lowpowereast <chuckliu1@hotmail.com>
2016-10-03 17:56:33 -0500

    Add files via upload

---
+++ 217. Contains Duplicate/Solution2_noextrastructure.py
@@ -0,0 +1,15 @@
+class Solution(object):
+def no_duplicates_no_structures(str):
+    
+""" Now without using additional data structures """
+
+    
+    for letter in str:
+        
+        if str.count(letter) > 1:
+            
+           return False
+    
+    else:
+        
+        return True
